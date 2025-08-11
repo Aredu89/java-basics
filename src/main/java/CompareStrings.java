a6a2cc143a97a0da42333165ded95ac6d81cd911
Aredu89 <aredu_r89@hotmail.com>
2025-08-11 17:21:33 -0500

    Switch statements

--- src/main/java/CompareStrings.java
+++ src/main/java/CompareStrings.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
 public class CompareStrings {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "Hello";
+
+        // Compare memory addresses
+        boolean result1 = (s1 == s2);
+        System.out.println("s1: " + s1);
+        System.out.println("s2: " + s2);
+        System.out.println("s1 == s2: " + result1);
+
+        // New memory address
+        String s3 = new String("Hello");
+        boolean result2 = (s1 == s3);
+
+        System.out.println("s1: " + s1);
+        System.out.println("s3: " + s3);
+        System.out.println("s1 == s3: " + result2);
+
+        // Compare string content
+        boolean result3 = s1.equals(s3);
+        System.out.println("s1 == s3 using .equals: " + result3);
+
+        // Compare strings using ignore case
+        String s4 = new String("hello");
+        System.out.println("s4: " + s4);
+        boolean result4 = s4.equalsIgnoreCase(s1);
+        System.out.println("s4.equalsIgnoreCase(s1): " + result4);
+    }
 }
--- src/main/java/SwitchStatements.java
+++ src/main/java/SwitchStatements.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class SwitchStatements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your computer type: ");
+        String computerType = scanner.nextLine();
+
+        switch(computerType) {
+            case "smartphone":
+                System.out.println("Smartphone computer type");
+                break;
+            case "tablet":
+                System.out.println("tablet computer type");
+                break;
+            default:
+                System.out.println("Computer type unknown");
+                break;
+        }
+
+        scanner.close();
+    }
 }
