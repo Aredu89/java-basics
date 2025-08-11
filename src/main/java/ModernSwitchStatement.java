a712ba1a2f3472d4f3ed742e39c63d5272b0b9c9
Aredu89 <aredu_r89@hotmail.com>
2025-08-11 17:50:26 -0500

    Add enum

--- src/main/java/ModernSwitchStatement.java
+++ src/main/java/ModernSwitchStatement.java
@@ -3,16 +3,28 @@ import java.util.Scanner;
 public class ModernSwitchStatement {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ComputerType myComputerType = null;
 
         System.out.print("Enter computer type: ");
         String computerType = scanner.nextLine();
 
-        computerType = computerType.toLowerCase();
+        // Convert the input to enum
+        computerType = computerType.trim().toUpperCase();
+        try {
+            myComputerType = ComputerType.valueOf(computerType);
+        }
+        catch(IllegalArgumentException exc) {
+            System.out.println("Invalid computer type entered: " + computerType);
+            System.exit(1);
+        }
+        finally {
+            scanner.close();
+        }
 
-        String description = switch(computerType) {
-            case "smartphone" -> "Smartphone computer type";
-            case "tablet" -> "tablet computer type";
-            case "desktop", "computer" -> "desktop computer type";
+        String description = switch(myComputerType) {
+            case ComputerType.SMARTPHONE -> "Smartphone computer type";
+            case ComputerType.TABLET -> "tablet computer type";
+            case ComputerType.DESKTOP, ComputerType.COMPUTER -> "desktop computer type";
             default -> "Unknown computer type";
         };
 
