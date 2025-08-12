ff7bcbff320cb02345453d1e6f9bf2adbdf48c28
Aredu89 <aredu_r89@hotmail.com>
2025-08-12 17:47:24 -0500

    While and Do While loops

--- src/main/java/ForLoops.java
+++ src/main/java/ForLoops.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class ForLoops {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //Nested for loops
         for (int row = 1; row <= 5; row++) {
 
             for(int col = 1; col <= 5; col++) {
@@ -8,5 +12,37 @@ public class ForLoops {
             }
             System.out.println();
         }
+
+        //While loops
+        boolean done = false;
+        while(!done) {
+            System.out.println("While Loop");
+            System.out.print("Are we done? Y/N: ");
+
+            String userInput = scanner.nextLine();
+
+            if(userInput.equalsIgnoreCase("Y")) {
+                done = true;
+            }
+
+            System.out.println();
+        }
+
+        // Do While loop
+        boolean done2 = false;
+        do {
+            System.out.println("Do While Loop");
+            System.out.print("Are we done? Y/N: ");
+
+            String userInput = scanner.nextLine();
+
+            if(userInput.equalsIgnoreCase("Y")) {
+                done2 = true;
+            }
+
+            System.out.println();
+        } while(!done2);
+
+        scanner.close();
     }
 }
