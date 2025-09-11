fb31da8395bcc471b6332b26904ff72290ca66dc
Aredu89 <aredu_r89@hotmail.com>
2025-09-11 17:28:11 -0500

    Encapsulate enemy properties.

--- BattleArena/src/main/java/Enemy.java
+++ BattleArena/src/main/java/Enemy.java
@@ -1,7 +1,23 @@
 public class Enemy {
-    String typeOfEnemy;
-    int healthPoints = 10;
-    int attackDamage = 1;
+    private String typeOfEnemy;
+    private int healthPoints = 10;
+    private int attackDamage = 1;
+
+    public String getTypeOfEnemy() {
+        return typeOfEnemy;
+    }
+
+    public void setTypeOfEnemy(String typeOfEnemy) {
+        this.typeOfEnemy = typeOfEnemy;
+    }
+
+    public int getHealthPoints() {
+        return healthPoints;
+    }
+
+    public int getAttackDamage() {
+        return attackDamage;
+    }
 
     public void talk() {
         System.out.println("I am a " + typeOfEnemy + " be prepared to fight!");
