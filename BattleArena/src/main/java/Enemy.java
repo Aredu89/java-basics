4a0abfd5c1eb9f9286fc2f13fe53971424337b47
Aredu89 <aredu_r89@hotmail.com>
2025-09-11 17:39:38 -0500

    Add more setters

--- BattleArena/src/main/java/Enemy.java
+++ BattleArena/src/main/java/Enemy.java
@@ -1,7 +1,7 @@
 public class Enemy {
     private String typeOfEnemy;
-    private int healthPoints = 10;
-    private int attackDamage = 1;
+    private int healthPoints;
+    private int attackDamage;
 
     public String getTypeOfEnemy() {
         return typeOfEnemy;
