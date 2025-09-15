76868557ab016f468d6007300603a08faacd0608
Aredu89 <aredu_r89@hotmail.com>
2025-09-15 17:54:29 -0500

    Create a new object

--- BattleArena/src/main/java/Main.java
+++ BattleArena/src/main/java/Main.java
@@ -1,10 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         Enemy zombie = new Enemy("Zombie", 10, 1);
+        Enemy ogre = new Enemy("Ogre", 20, 3);
+
         System.out.println(zombie.getTypeOfEnemy() + " has " + zombie.getHealthPoints() + " health points and can do an attack of " + zombie.getAttackDamage());
+        System.out.println(ogre.getTypeOfEnemy() + " has " + ogre.getHealthPoints() + " health points and can do an attack of " + ogre.getAttackDamage());
 
         zombie.talk();
         zombie.walkForward();
         zombie.attack();
+
+        ogre.talk();
+        ogre.walkForward();
     }
 }
