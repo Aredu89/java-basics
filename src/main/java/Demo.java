31bfea59babe16d28052d271e02e3e1924a60f0e
Aredu89 <aredu_r89@hotmail.com>
2025-11-10 17:21:29 -0600

    HashSet

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -8,6 +8,18 @@ public class Demo {
         System.out.println("Favorite Color: Blue");
         System.out.println("Hobby: Coding");
 
+        // HashSets
+        var books = new HashSet<>(List.of("Book 0"));
+        books.add("Book 1");
+        books.add("Book 2");
+        books.add("Book 3");
+
+        books.remove("Book 2");
+        System.out.println(books.contains("Book 3"));
+
+        for (String book: books) {
+            System.out.println(book);
+        }
     }
 
 }
