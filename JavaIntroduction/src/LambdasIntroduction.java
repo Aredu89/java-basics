95cdedebc1869803a185e20929b2685b1b5e6c09
Aredu89 <aredu_r89@hotmail.com>
2025-12-23 17:21:29 -0600

    Lambda Functional Interface

--- JavaIntroduction/src/LambdasIntroduction.java
+++ JavaIntroduction/src/LambdasIntroduction.java
@@ -3,15 +3,12 @@ import java.util.List;
 
 public class LambdasIntroduction {
     public static void main(String[] args) {
-        List<String> names = new ArrayList<>();
-        names.add("Edgar");
-        names.add("Ariel");
-        names.add("Enzo");
-        names.add("Ivan");
-        names.add("Lucas");
-        names.add("Joaquin");
-        names.add("David");
-
-        names.forEach((n) -> System.out.println(n));
+        Greetings lambda = (n) -> System.out.println(n);
+        lambda.greetings("Hello java!");
     }
+}
+
+@FunctionalInterface
+interface Greetings {
+    void greetings(String greeting);
 }
