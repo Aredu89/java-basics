3097ba196bfd4a8a039db1f37bba5eb02d2a1423
Aredu89 <aredu_r89@hotmail.com>
2026-01-05 18:19:10 -0600

    Try and catch in lambdas

--- JavaIntroduction/src/LambdasIntroduction.java
+++ JavaIntroduction/src/LambdasIntroduction.java
@@ -28,6 +28,17 @@ public class LambdasIntroduction {
 
         boolean result = lessThan100.and(greaterThan50).test(55);
         System.out.println(result);
+
+        Calculate divide = (a1,b2) -> {
+            try {
+                return a1 / b2;
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+                return -1;
+            }
+        };
+        int solution = divide.perform(10,2);
+        System.out.println(solution);
     }
 }
 
@@ -44,4 +55,9 @@ interface StringEndings {
 @FunctionalInterface
 interface StringCompare {
     String perform (String s1, String s2);
+}
+
+@FunctionalInterface
+interface Calculate {
+    int perform(int a, int b);
 }
