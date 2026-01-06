1184570e1378f37337bfbb87a59eeab43b300f27
Aredu89 <aredu_r89@hotmail.com>
2026-01-05 18:11:57 -0600

    Lambdas and Predicates

--- JavaIntroduction/src/LambdasIntroduction.java
+++ JavaIntroduction/src/LambdasIntroduction.java
@@ -1,14 +1,47 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class LambdasIntroduction {
     public static void main(String[] args) {
         Greetings lambda = (n) -> System.out.println(n);
         lambda.greetings("Hello java!");
+
+        StringEndings exclamationMark = (s) -> s + "!";
+        System.out.println(exclamationMark.perform("Hello"));
+
+        String a = "Ariel";
+        String b = "Developer";
+
+        StringCompare value = (s1, s2) -> {
+            if(s1.length() > s2.length()) {
+                return s1;
+            }
+            return s2;
+        };
+
+        String longerWord = value.perform(a, b);
+        System.out.println(longerWord);
+
+        Predicate<Integer> lessThan100 = i -> (i<100);
+        Predicate<Integer> greaterThan50 = i -> (i>50);
+
+        boolean result = lessThan100.and(greaterThan50).test(55);
+        System.out.println(result);
     }
 }
 
 @FunctionalInterface
 interface Greetings {
     void greetings(String greeting);
+}
+
+@FunctionalInterface
+interface StringEndings {
+    String perform(String s);
+}
+
+@FunctionalInterface
+interface StringCompare {
+    String perform (String s1, String s2);
 }
