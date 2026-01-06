4ee587e7b661888aac35975513840de84115e8a2
Aredu89 <aredu_r89@hotmail.com>
2026-01-06 17:46:52 -0600

    Streams with objects

--- JavaIntroduction/src/Streams.java
+++ JavaIntroduction/src/Streams.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,5 +14,24 @@ public class Streams {
                 .collect(Collectors.toList());
 
         result.forEach(System.out::println);
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(1, "Eric", 8));
+        employees.add(new Employee(2, "Milo", 5));
+        employees.add(new Employee(3, "Melissa", 12));
+        employees.add(new Employee(4, "Elijah", 1));
+        employees.add(new Employee(5, "Adil", 24));
+        employees.add(new Employee(6, "Enrique", 1));
+        employees.add(new Employee(7, "Chad", 18));
+
+        long employeeYearsAboveTen = employees.stream()
+                .filter(e -> e.getYearsOfService() > 10)
+                .count();
+        System.out.println(employeeYearsAboveTen);
+
+        List<Employee> namesStartWithE = employees.stream()
+                .filter(e -> e.getFirstName().startsWith("E"))
+                .toList();
+        namesStartWithE.forEach(e -> System.out.println(e.getFirstName()));
     }
 }
